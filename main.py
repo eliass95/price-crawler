2bdedc1169d83825dbf8a118bfc661fbae2ab488
Elias Soares <elias_soares95@hotmail.com>
2016-09-26 23:53:39 -0300

    Script dentro do loop.

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from urllib.request import Request, urlopen
 import re
 
-#funciona submarino e americanas
-url = input("Url do produto: ")
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = urlopen(req).read().decode('utf-8')
+while True:
+    #funciona submarino e americanas
+    url = input("URL DO PRODUTO: ")
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urlopen(req).read().decode('utf-8')
 
-produto = r"description\" content=\"(.*?)\""
-preco = r"price/salesPrice\".*?>(.*?)</span>"
+    produto = r"description\" content=\"(.*?)\""
+    preco = r"price/salesPrice\".*?>(.*?)</span>"
 
-print()
+    print()
 
-# Coleta o nome do produto
-regex = re.compile(produto)
-matches = re.findall(regex, html)
-print("PRODUTO >> " + matches[0])
+    # Coleta o nome do produto
+    regex = re.compile(produto)
+    matches = re.findall(regex, html)
+    print("PRODUTO >> " + matches[0])
 
-# Coleta o preço do produto
-regex = re.compile(preco)
-matches = re.findall(regex, html)
-print("PRECO >> " + matches[0])
+    # Coleta o preço do produto
+    regex = re.compile(preco)
+    matches = re.findall(regex, html)
+    print("PRECO >> " + matches[0])
+
+    print("\n--------------------------------------\n")
